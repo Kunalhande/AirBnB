921880153f18bc8db11071cd3051e98e3725d588
kunal hande <kunalhand30@gmail.com>
2025-09-19 06:53:30 +0530

    Remove history files from repo

--- .history/utils/ExpressError_20250910021352.js
+++
@@ -1,9 +0,0 @@
-class ExpressError extends Error{
-    constructor(status,message) {
-        super();
-        this.statusCode = this.statusCode;
-        this.message = message;
-    }
-}
-
-module.exports = ExpressError;
