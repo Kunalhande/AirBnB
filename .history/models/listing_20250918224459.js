f362617e7abe54b772cd1464e082c8166c39be39
kunal hande <kunalhand30@gmail.com>
2025-09-20 23:45:55 +0530

    just daily update

---
+++ .history/models/listing_20250918224459.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const listingSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: String,
+  image: {
+    url: String,
+    filename: String
+  },
+  price: Number,
+  location: String,
+  country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    }
+  ]
+});
+
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
