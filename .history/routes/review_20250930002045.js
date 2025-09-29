8f5514486224408908cddedf625fe778ebe6174d
kunal hande <kunalhand30@gmail.com>
2025-09-30 00:27:29 +0530

    commit for the flash.ejs creation

---
+++ .history/routes/review_20250930002045.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router= express.Router({ mergeParams: true});
+const wrapAsync = require("../utils/wrapAsync");
+const ExpressError = require("../utils/ExpressError");
+const { listingSchema, reviewSchema  } =require("../schema.js");
+const Review = require("../models/review");
+const Listing = require("../models/listing");
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el => el.message).join(",");
+    throw new ExpressError(400, msg);
+  } else {
+    next();
+  }
+};
+
+//reviews
+//post review-route
+router.post("/",validateReview, wrapAsync(async (req, res) => {
+  let listing = await Listing.findById(req.params.id); // Use model 'Listing'
+  let newReview = new Review(req.body.review);         // Use model 'Review'
+
+  listing.reviews.push(newReview);
+
+  await newReview.save();
+  await listing.save();
+  req.flash("success", "New review created !");
+  res.redirect(`/listings/${listing._id}`);
+}));
+
+//Delete  REview-Route
+router.delete("/:reviewId", wrapAsync(async(req,res) =>{
+    let {id, reviewId } =req.params;
+
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    req.flash("success", "New review Deleted !");
+    res.redirect(`/listings/${id}`)
+}));
+
+module.exports = router;
