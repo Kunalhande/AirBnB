39ecc153ca9eb552e5aeb9b3ce506193ebd37f42
kunal hande <kunalhand30@gmail.com>
2025-10-02 11:24:51 +0530

    commit for the router updation

--- .history/app_20251002112054.js
+++ .history/app_20251002112054.js
@@ -12,9 +12,9 @@ const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
 
-const listingRouter = require("./routes/listing.js");
-const reviewRouter = require("./routes/review.js");
-const userRouter = require("./routes/user.js");
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
+const reviews = require("./routes/user.js");
 
 
 const MONGO_URL ="mongodb://127.0.0.1:27017/test";
@@ -93,9 +93,8 @@ app.get("/demouser", async(req,res) =>{
    res.send(registeredUser);
 });
 
-app.use("/listings", listingRouter);
-app.use("/listings/:id/reviews", reviewRouter);
-app.use("/", userRouter);
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews)
 
 
 // app.use((err,req,res,next) =>{
